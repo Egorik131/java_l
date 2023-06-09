88d0155515e956ac81c84b11c9f54352517278f9
*Egor <egorik13@gmail.com>
2023-06-09 17:51:22 +0300

    upd: seminar

--- src/main.java
+++
@@ -1,9 +0,0 @@
-public class Main {
-    public static int Sum(int N){
-        int res = 0; // 0(1)
-        for (int i =1; i <= N; i++){
-            res += i
-        }
-        return res;
-    }
-}
